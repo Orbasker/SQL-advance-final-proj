615a338182ae957dba37d2604015b9292c363c41
Nikolai-Gith <mnickslm7@gmail.com>
2025-03-02 13:02:05 +0200

    Finished 1
    filled missing JSON values

--- sql/new.sql
+++ sql/new.sql
@@ -153,6 +153,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
+-- TEST the fill script for missing JSON logs
 SELECT id, custom_fields, 
        json_build_object(
            'user_id', COALESCE(user_id::TEXT, 'null'),
@@ -160,4 +161,13 @@ SELECT id, custom_fields,
            'timestamp', COALESCE(timestamp::TEXT, 'null')
        ) AS new_custom_fields
 FROM logs
+WHERE custom_fields IS NULL OR custom_fields::TEXT = '{}';
+
+-- fill script for missing JSON logs
+UPDATE logs
+SET custom_fields = json_build_object(
+    'user_id', COALESCE(user_id::TEXT, 'null'),
+    'action', COALESCE(action, 'no_data'),
+    'timestamp', COALESCE(timestamp::TEXT, 'null')
+)
 WHERE custom_fields IS NULL OR custom_fields::TEXT = '{}';
